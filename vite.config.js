9f762a509c97c295f0de1c083622de9581e14ef7
alokabhinandan <alokabhinandan123@gmail.com>
2025-04-16 23:44:25 +0530

    Update code with latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/anand-project2/', // This is the correct syntax for setting the base URL for GitHub Pages
   server: { port: 5173 }
 })
